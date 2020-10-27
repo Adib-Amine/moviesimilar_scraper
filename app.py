540dd4ee02b6613503a5a267a68ae1de5edda476
Adib-Amine <amineadib007@gmail.com>
2020-10-27 23:55:37 +0100

    scraping

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from fastapi import FastAPI
 import uvicorn
 import urllib3
-
+from bs4 import BeautifulSoup
 
 http = urllib3.PoolManager()
 url = "https://www.movie-map.com/"
 movie_input  = input(" write movie name : ")
 movie_query = movie_input.replace(' ',"+") 
 resp = http.request('GET', url+movie_query)
-data = print(resp.data.decode('utf-8'))
-
+data = resp.data
+reponse_html = BeautifulSoup(data, 'html.parser')
+for link in reponse_html.findAll("a", {"class": "S"}):
+    print(link.text)
 
 # app = FastAPI(debug=True)
 
